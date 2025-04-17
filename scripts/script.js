81e9b9711710beed4e372c31adc90deef23458b6
Dylan Dang <dyland9576@gmail.com>
2025-04-17 13:34:13 -0600

    adds missing javascript lines

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** YOUR NAME: Dylan Dang
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -11,7 +11,8 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
-
+let modelName = "XYZ";
+let duration = 1;
 
 
 
@@ -27,7 +28,18 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
-
+function recalculate() {
+    let costLabel = document.getElementById("calculated-cost");
+    let totalCost;
+    
+    if (modelName === "XYZ") {
+        totalCost = duration * 100;
+    } else if (modelName === "CPRG") {
+        totalCost = duration * 213;
+    }
+    
+    costLabel.innerHTML = totalCost;
+}
 
 
 
@@ -46,7 +58,23 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+let modelButton = document.getElementById("model-button");
 
+function changeModel() {
+    let modelText = document.getElementById("model-text");
+    
+    if (modelName === "XYZ") {
+        modelName = "CPRG";
+        modelText.innerHTML = "Model CPRG";
+    } else if (modelName === "CPRG") {
+        modelName = "XYZ";
+        modelText.innerHTML = "Model XYZ";
+    }
+    
+    recalculate();
+}
+
+modelButton.addEventListener("click", changeModel);
 
 
 
@@ -65,5 +93,18 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+let durationButton = document.getElementById("duration-button");
 
+function changeDuration() {
+    let durationText = document.getElementById("duration-text");
+    let newDuration = prompt("Enter new duration:");
+
+    if (newDuration !== null && !isNaN(newDuration)) {
+        duration = parseInt(newDuration);
+        durationText.innerHTML = duration;
+        recalculate();
+    }
+}
+
+durationButton.addEventListener("click", changeDuration);
 
